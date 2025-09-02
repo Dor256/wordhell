ab7089a50d0efe9f1a370dd3103eee1f3d9b26a2
Dor256 <dorb@wix.com>
2025-09-02 23:26:17 +0300

    make guesses persistent

--- client/src/LocalStorage.elm
+++ client/src/LocalStorage.elm
@@ -1,10 +1,9 @@
 port module LocalStorage exposing (..)
 
-
 port saveString : ( String, String ) -> Cmd msg
-
 
 port onLoad : String -> Cmd msg
 
-
 port loadString : (Maybe String -> msg) -> Sub msg
+
+port deleteItem : String -> Cmd msg
